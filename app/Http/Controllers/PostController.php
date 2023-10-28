b6ab1202f9d1d69ce38fd5e6852f652d75ea24b0
ryuari0424 <ryu.ari.0424@gmail.co.jp>
2023-10-28 14:25:40 +0000

    hensyuu

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     //
+
+    public function index()
+    {
+        $user = Auth::user();
+        $posts = $user->posts;
+
+        return view(
+            'index',
+            [
+                'user' => $user,
+                'posts' => $posts
+            ]
+        );
+    }
+    public function createPost()
+    {
+    }
 }
